7165ff3877b30c5ff740dc5a39fb1f42e58649ed
YiFeng <yif12@uci.edu>
2023-02-11 19:48:39 -0800

    revised vector addition, use struct

--- week4/matrix/vector.c
+++ week4/matrix/vector.c
@@ -4,11 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-int LENGTH_VECTOR;
-void initialize_vetors(int *vector_1, int *vector_2);
-bool is_all_digits(char *s);
-int* array_addition(int *vector_1, int *vetor_2);
+typedef struct
+{
+    int* vec;
+    int len;
+}
+Vector;
 
+Vector initialize_vetors(int len_vector);
+bool is_number(char *s);
+Vector vector_addition(Vector vector_1, Vector vector_2);
+void print_vector(Vector vec_cur);
 
 int main(int argc, char *argv[])
 {
@@ -19,30 +25,31 @@ int main(int argc, char *argv[])
         return 1;
     }
 
-    // Check if a string has non-digits
-    if (!is_all_digits(argv[1]))
-    {
-        printf("Please anter valid number of vector length");
+    // Convert argument into int
+   int len_vector = atoi(argv[1]);
+   if (len_vector == 0)
+   {
+        printf("Please enter valid number of vector length \n");
         return 1;
-    }
+   }
 
-    LENGTH_VECTOR = atoi(argv[1]);
+    // Initialize two vectors
+    Vector vector_1 = initialize_vetors(len_vector);
+    Vector vector_2 = initialize_vetors(len_vector);
 
-    // Initialize two arrays
-    int vector_1[LENGTH_VECTOR];
-    int vector_2[LENGTH_VECTOR];
+    // Add two vectors and print
+    Vector sum_vectors = vector_addition(vector_1, vector_2);
+    free(vector_1.vec);
+    free(vector_2.vec);
 
-    initialize_vetors(vector_1, vector_2);
+    // Print sum of two vectors
+    print_vector(sum_vectors);
 
-
-    // Add two arrays and print
-    int *sum_arrays = array_addition(vector_1, vector_2);
-    free(sum_arrays);
-
+    free(sum_vectors.vec);
     return 0;
 }
 
-bool is_all_digits(char *s)
+bool is_number(char *s)
 {
     int length = strlen(s);
     for (int i = 0; i < length; i++)
@@ -55,31 +62,39 @@ bool is_all_digits(char *s)
     return true;
 }
 
-void initialize_vetors(int *vector_1, int *vector_2)
+Vector initialize_vetors(int len_vector)
 {
-    for (int i = 0; i < LENGTH_VECTOR; i++)
-    {
-        printf("initialize vector 1: \n");
-        scanf("%d", &vector_1[i]);
-    }
+    Vector vec_cur;
+    vec_cur.vec = calloc(len_vector, sizeof(int));
+    vec_cur.len = len_vector;
 
-    for (int i = 0; i < LENGTH_VECTOR; i++)
+    for (int i = 0; i < len_vector; i++)
     {
-        printf("initialize vector 2: \n");
-        scanf("%d", &vector_2[i]);
+        printf("initialize vector element %i: \n", i+1);
+        scanf("%d", &vec_cur.vec[i]);
     }
-
+    return vec_cur;
 }
 
-int* array_addition(int *vector_1, int *vector_2)
+Vector vector_addition(Vector vector_1, Vector vector_2)
 {
-    int *sum_arrays= malloc(LENGTH_VECTOR * sizeof(int));;
+    int len_vec = vector_1.len;
+    Vector sum_vectors;
+    sum_vectors.len = len_vec;
+    sum_vectors.vec = calloc(len_vec, sizeof(int));
 
-    for (int i = 0; i < LENGTH_VECTOR; i++)
+    for (int i = 0; i < len_vec; i++)
     {
-        sum_arrays[i] = vector_1[i] + vector_2[i];
-        printf("%i,", sum_arrays[i]);
+        sum_vectors.vec[i] = vector_1.vec[i] + vector_2.vec[i];
+    }
+    return sum_vectors;
+}
+
+void print_vector(Vector vec_cur)
+{
+    for (int i = 0; i < vec_cur.len; i++)
+    {
+        printf("%d, ", vec_cur.vec[i]);
     }
     printf("\n");
-    return sum_arrays;
 }
