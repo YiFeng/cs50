a4f046beb0e8a2ef099cb4ea187dff929e9149c6
YiFeng <yif12@uci.edu>
2023-02-23 00:08:38 -0800

    Yujian revise grammar to simplify code

--- week5/speller/dictionary.c
+++ week5/speller/dictionary.c
@@ -3,39 +3,34 @@
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 #include <strings.h>
 
 #include "dictionary.h"
 
 // Represents a node in a hash table
-typedef struct node
-{
+typedef struct Node {
     char word[LENGTH + 1];
-    struct node *next;
-}
-node;
+    struct Node *next;
+} Node;
 
 // TODO: Choose number of buckets in hash table
-const unsigned int N = 26 * 2;
+const unsigned int BUCKET_SIZE = 26 * 2;
 
 // Hash table
-typedef struct HashTable
-{
-    node *table[N];
-    int size;
-}
-HashTable;
+typedef struct {
+    Node *buckets[BUCKET_SIZE];
+    unsigned int size;
+} HashTable;
 
-HashTable *table;
+HashTable table = {{NULL}, 0};
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
 {
-    // TODO
-    int index = hash(word);
-    node *curr_node = table->table[index];
+    unsigned int index = hash(word);
+    Node *curr_node = table.buckets[index];
     while (curr_node != NULL)
     {
         if(strcasecmp(curr_node->word, word) == 0)
@@ -51,26 +46,17 @@ bool check(const char *word)
 unsigned int hash(const char *word)
 {
     // get word's position in hash table
-    int word_type;
-    if (strlen(word) <= 6)
-    {
-        word_type = 0;
-    }
-    else
-    {
-        word_type = 1;
-    }
+    int word_type = (int) strlen(word)> 6;
     int first_letter = tolower(word[0]) - 'a';
+    // TODO handle special case, like empty string, or strange char.
     unsigned int index = first_letter * 2 + word_type;
-
     return index;
 }
 
 bool save_to_hash(char *buffer)
 {
-    node *vocable = calloc(1, sizeof(node));
-    if (vocable == NULL)
-    {
+    Node *vocable = calloc(1, sizeof(Node));
+    if (vocable == NULL) {
         return false;
     }
     strcpy(vocable->word, buffer);
@@ -78,8 +64,9 @@ bool save_to_hash(char *buffer)
 
     // get word's position in hash table
     unsigned int index = hash(buffer);
-    vocable->next = table->table[index];
-    table->table[index] = vocable;
+    if (index < 0 || index >= BUCKET_SIZE) return false;
+    vocable->next = table.buckets[index];
+    table.buckets[index] = vocable;
     return true;
 }
 
@@ -88,64 +75,38 @@ bool load(const char *dictionary)
 {
     // open the file of dictionary
     FILE *file = fopen(dictionary, "r");
-    if (file == NULL)
-    {
+    if (file == NULL) {
         printf("Could not open %s.\n", dictionary);
         return false;
     }
-    // read word from dictionary and save to hashtable
-    table = malloc(sizeof(HashTable));
-    if (table == NULL)
-    {
-        return false;
-    }
-    table->size = 0;
 
+    // read word from dictionary and save to hashtable
     char buffer[LENGTH + 2];
-    while (fgets(buffer, LENGTH+2, file))
-    {
-        if(!save_to_hash(buffer))
-        {
+    while (fgets(buffer, LENGTH + 2, file)) {
+        if(!save_to_hash(buffer)) {
             return false;
         }
-        else
-        {
-            table->size++;
-        }
+        table.size++;
     }
     return true;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
-unsigned int size(void)
-{
-    return table->size;
+unsigned int size(void) {
+    return table.size;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
-bool unload(void)
-{
-    // TODO
-    // free elements of hash array
-    if (table == NULL)
-    {
-        return false;
-    }
-    for (int i = 0; i < N; i++)
-    {
-        node *curr_node = table->table[i];
-        if (curr_node == NULL)
-        {
-            continue;
-        }
-        while (curr_node != NULL)
-        {
-            node *next_node = curr_node->next;
+bool unload(void) {
+    // free elements of hash table array
+    for (int i = 0; i < BUCKET_SIZE; i++) {
+        Node *curr_node = table.buckets[i];
+        while (curr_node != NULL) {
+            Node *next_node = curr_node->next;
             free(curr_node);
+            table.size--;
             curr_node = next_node;
         }
     }
-    // free the table
-    free(table);
     return true;
 }
