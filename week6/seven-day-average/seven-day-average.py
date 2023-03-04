d4a574c8267fe2d0b5978f8841f1034fcddc601d
YiFeng <yif12@uci.edu>
2023-03-03 18:09:12 -0800

    finish practice dict about covid cases

--- week6/seven-day-average/seven-day-average.py
+++ week6/seven-day-average/seven-day-average.py
@@ -35,15 +35,40 @@ def main():
 # TODO: Create a dictionary to store 14 most recent days of new cases by state
 def calculate(reader):
     recent2weeks_cases = {}
-    day_case = {}
+    previous_cases = {}
     for row in reader:
-        row['state']
+        state = row['state']
+        num_case = int(row['cases'])
 
+        if state not in recent2weeks_cases:
+            recent2weeks_cases[state] = []
+        if len(recent2weeks_cases[state]) >= 14:
+            recent2weeks_cases[state].pop(0)
 
+        if state in previous_cases:
+            recent2weeks_cases[state].append(num_case - previous_cases[state])
+        else:
+            recent2weeks_cases[state].append(num_case)
+        previous_cases[state] = num_case
+    return recent2weeks_cases
 
 # TODO: Calculate and print out seven day average for given state
 def comparative_averages(new_cases, states):
-    ...
+    for state in states:
+        this_week = sum(new_cases[state][7:14]) / 7
+        last_week = sum(new_cases[state][0:7]) / 7
+
+        try:
+            change = (this_week - last_week)/last_week * 100
+            if change > 0:
+                print(f"{state} had a 7-day average of {this_week:.0f} and an increase of {change:.0f}%")
+            elif change < 0:
+                print(f"{state} had a 7-day average of {this_week:.0f} and an decrease of {-change:.0f}%")
+            else:
+                print(f"{state} had a 7-day average of {this_week:.0f} and same as last week")
+        except ZeroDivisionError:
+            print("last week has 0 cases")
+
 
 
 main()
