60935e01c7904b8f320f04c5da8ea96f30913bcc
YiFeng <yif12@uci.edu>
2023-03-12 15:56:23 -0700

    finish debug, within database any table can access primary key

--- week7/prophecy/students.py
+++ week7/prophecy/students.py
@@ -4,8 +4,8 @@ from cs50 import SQL
 db = SQL("sqlite:///roster.db")
 
 db.execute('CREATE TABLE IF NOT EXISTS students_new (id INTEGER PRIMARY KEY, student_name TEXT)')
-db.execute('CREATE TABLE IF NOT EXISTS houses (house TEXT UNIQUE PRIMARY KEY, head TEXT)')
-# db.execute('CREATE TABLE IF NOT EXISTS house_assign (student_id INTEGER PRIMARY KEY, house TEXT, FOREIGN KEY(house) REFERENCES houses(house)')
+db.execute('CREATE TABLE IF NOT EXISTS houses (id INTEGER PRIMARY KEY, house TEXTs, head TEXT)')
+db.execute('CREATE TABLE IF NOT EXISTS house_assign (id INTEGER PRIMARY KEY, student_name TEXT, house TEXT)')
 
 with open("students.csv", "r") as file:
     reader = csv.DictReader(file)
@@ -14,11 +14,8 @@ with open("students.csv", "r") as file:
         name = row['student_name']
         house = row['house']
         head = row['head']
-        db.execute("INSERT INTO students_new (id) VALUES (?)", id)
         db.execute("INSERT INTO students_new (student_name) VALUES (?)", name)
-        db.execute("INSERT INTO houses (house) VALUES (?)", house)
-        db.execute("INSERT INTO houses (head) VALUES (?)", head)
-        # db.execute("INSERT INTO house_assign (student_id) VALUES (?)", id)
-        # db.execute("INSERT INTO house_assign (house) VALUES (?)", house)
+        db.execute("INSERT INTO houses (house, head) VALUES (?,?)", house, head)
+        db.execute("INSERT INTO house_assign (student_name, house) VALUES (?, ?)", name, house)
 
 
