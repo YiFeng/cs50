15ac7387235f4513a9b54540d652c93996ba19c6
YiFeng <yif12@uci.edu>
2023-03-12 22:14:47 -0700

    finish week7 sql movies, got pass

---
+++ week7/movies/11.sql
@@ -0,0 +1,7 @@
+SELECT m.title
+FROM movies m
+JOIN ratings r ON m.id =r.movie_id
+JOIN stars s ON r.movie_id = s.movie_id
+JOIN people p ON s.person_id = p.id
+WHERE p.name = "Chadwick Boseman"
+ORDER BY r.rating DESC LIMIT 5;
---
+++ week7/movies/4.sql
@@ -0,0 +1,3 @@
+SELECT COUNT(*) as n
+FROM ratings
+WHERE rating = 10.0;
