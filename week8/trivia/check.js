992e4dca0e87cef82b1a8ee6b08c5ebdb8a436b5
YiFeng <yif12@uci.edu>
2023-04-02 11:48:46 -0700

    put all control part in js, learn eventlistener

--- week8/trivia/check.js
+++ week8/trivia/check.js
@@ -1,23 +1,39 @@
-function choiceAnswer(button) {
-    if (button.value == 'correct'){
+function choiceAnswer(button, event) {
+    if (button.value.toLowerCase() === 'zelda') {
         button.style.backgroundColor='green';
         document.querySelector('#result_choice').textContent='Correct!';
-    }
-    else{
+    } else {
         button.style.backgroundColor='red';
         document.querySelector('#result_choice').textContent='InCorrect!';
     }
-    document.querySelectorAll('button').forEach(function(btn) {
-    btn.disabled = true;});
 }
 
-function inputAnswer(){
+window.addEventListener('load', () => {
+    const buttons = document.querySelectorAll('button');
+    console.log(buttons);
+    for (const button of buttons) {
+        if (button.id === "text_check") continue;
+        button.addEventListener("click", (event) => {
+            choiceAnswer(button, event);
+        });
+    }
+});
+
+/*choiceAnswer.bind(button)*/
+/*
+button.addEventListener = function(eventType, callback) {
+    if (currentEvent.type === eventType) {
+        callback(currentEvent);
+    }
+}
+*/
+
+function inputAnswer() {
     var inputtext = document.querySelector('input').value
-    if (inputtext.toLowerCase() == 'zelda'){
+    if (inputtext.toLowerCase() == 'zelda') {
         document.querySelector('#result_input').textContent='Correct!'
         document.querySelector('#result_input').style.color='green' 
-    }
-    else{
+    } else {
         document.querySelector('#result_input').textContent='InCorrect!'
         document.querySelector('#result_input').style.color='red'
     }
